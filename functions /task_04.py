662c08c9c43205639cac92c73ad0f3e0dd343adc
DeneroCvach <denzelcvach@gmail.com>
2023-06-18 21:09:10 +0300

    4 task

--- functions /task_04.py
+++ functions /task_04.py
@@ -1,30 +1,42 @@
 # Напишите программу для вычисления квадратных уравнений.
 
-def quadratic_equation(a, b, c):
-    if a == 0:
-        return None
+def solve_quadratic_equation(a, b, c):
+    # Вычисляем дискриминант
+    discriminant = (b**2) - (4*a*c)
 
-    d = (b ** 2) - (4 * a * c)
+    if discriminant > 0:
+        x1 = (-1 * b + (discriminant ** 0.5)) / (2*a)
+        x2 = (-1 * b - (discriminant ** 0.5)) / (2*a)
 
-    if d != 0:
-        x1 = (-b + d ** 0.5) / (2 * a)
-        x2 = (-b - d ** 0.5) / (2 * a)
         return x1, x2
 
-    elif d == 0:
-        x = -b / (2 * a)
-        return x
+    elif discriminant == 0:
+        x = -b / (2*a)
+
+        return x, x
+
     else:
-        return None
+        return 0, 0
 
 
 def main():
-    equation_data = quadratic_equation(5, 3, -26)
+    a = float(input("Enter a: "))
+    b = float(input("Enter b: "))
+    c = float(input("Enter c: "))
 
-    if equation_data is None:
-        print('no solutions...')
+    solution1, solution2 = solve_quadratic_equation(a, b, c)
+
+    if solution1 == 0 and solution2 == 0:
+        print("the equation has no solutions: ")
+
+    elif solution1 == solution2:
+        print("the equation has one solution: ")
+        print("x =", solution1)
+
     else:
-        print('the solution(s) to the equation is', *equation_data)
+        print("the equation has two solutions: ")
+        print("x1 =", solution1)
+        print("x2 =", solution2)
 
 
 if __name__ == '__main__':
