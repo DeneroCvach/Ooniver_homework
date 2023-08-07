07a17ae506602e38057fae76cb49d0ead273730e
DeneroCvach <denzelcvach@gmail.com>
2023-08-07 21:55:45 +0300

    class work functional programming

---
+++ functional_programming/class_work/iterator.py
@@ -0,0 +1,10 @@
+num_list = [x for x in range(5)]
+# for i in num_list:
+#     print(i)
+
+itr = iter(num_list)
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
