e7658955461dc0e0a4400dc9f67bfb24181146de
DeneroCvach <denzelcvach@gmail.com>
2023-08-29 16:07:21 +0300

    factorial

--- flask_web/first_flask_app.py
+++ flask_web/first_flask_app.py
@@ -4,33 +4,33 @@ from datetime import datetime
 app = Flask(__name__)
 
 
-@app.route("/first_flask_app/<string:name>")
+@app.route("/hello_user/<string:name>")
 def hello_user(name):
     date = datetime.now()
     return f"<p>Hello, {name}! You have entered the site at {date}.</p>"
 
 
-@app.route("/first_flask_app/factorial/int:<number>")
-def factorial_calculation(number):
-    if type(number) == int:
+@app.route("/factorial/<number>")
+def factorial_calculation(number: str):
+    if number.isdigit():
+        number = int(number)
         result = factorial(number)
-        return f"<p>{result}</p>"
+        return f"<p>{result}</p>" if len(str(result)) < 20 else redirect(url_for("hello_user", name='Guest'))
     else:
-        return redirect(url_for("exception"))
+        return redirect(url_for("exception", number=number))
 
 
-@app.route("/exception")
-def exception():
-    raise TypeError("Enter number, not string!")
+@app.route("/exception/<number>")
+def exception(number):
+    return f"{number} - not a number. Enter number, not string!"
 
 
 def factorial(number):
     if number < 2:
         return 1
     else:
-        return number * factorial_calculation(number - 1) if len(str(number)) < 20 \
-            else redirect(url_for("first_flask_app"))
+        return number * factorial(number - 1)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
